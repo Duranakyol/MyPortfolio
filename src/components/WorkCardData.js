bbf776a4ba828da8561eb7f3933328281c7be7a4
abc <duranakyol71@gmail.com>
2023-02-03 16:02:26 +0100

    add WorkCard

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+
+const WorkCardData = [
+  {
+    imgsrc: pro1,
+    title: "E Commerce Web",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati officiis voluptate, error vero quidem dolor tempore eius in. Quo, pariatur? Expedita deleniti perferendis adipisci facilis ullam nostrum illo eveniet cumque? Sunt voluptatum nihil suscipit molestias quo? Labore neque aliquam unde nesciunt at quo provident eligendi vel quidem deleniti pariatur consequatur, temporibus laborum, consectetur sunt saepe maiores tempora ex ipsa, similique consequuntur nemo. Ad blanditiis excepturi delectus vitae, explicabo, pariatur assumenda cumque aliquam deleniti perferendis perspiciatis iusto? Sapiente, aspernatur eveniet dicta voluptates dolore facilis cumque corporis beatae, quia reprehenderit vero sint magnam, deserunt obcaecati praesentium ut doloribus reiciendis nesciunt unde temporibus!",
+    view: "Link at buraya",
+  },
+  {
+    imgsrc: pro2,
+    title: "E Commerce Web",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati officiis voluptate, error vero quidem dolor tempore eius in. Quo, pariatur? Expedita deleniti perferendis adipisci facilis ullam nostrum illo eveniet cumque? Sunt voluptatum nihil suscipit molestias quo? Labore neque aliquam unde nesciunt at quo provident eligendi vel quidem deleniti pariatur consequatur, temporibus laborum, consectetur sunt saepe maiores tempora ex ipsa, similique consequuntur nemo. Ad blanditiis excepturi delectus vitae, explicabo, pariatur assumenda cumque aliquam deleniti perferendis perspiciatis iusto? Sapiente, aspernatur eveniet dicta voluptates dolore facilis cumque corporis beatae, quia reprehenderit vero sint magnam, deserunt obcaecati praesentium ut doloribus reiciendis nesciunt unde temporibus!",
+    view: "Link at buraya",
+  },
+  {
+    imgsrc: pro3,
+    title: "E Commerce Web",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati officiis voluptate, error vero quidem dolor tempore eius in. Quo, pariatur? Expedita deleniti perferendis adipisci facilis ullam nostrum illo eveniet cumque? Sunt voluptatum nihil suscipit molestias quo? Labore neque aliquam unde nesciunt at quo provident eligendi vel quidem deleniti pariatur consequatur, temporibus laborum, consectetur sunt saepe maiores tempora ex ipsa, similique consequuntur nemo. Ad blanditiis excepturi delectus vitae, explicabo, pariatur assumenda cumque aliquam deleniti perferendis perspiciatis iusto? Sapiente, aspernatur eveniet dicta voluptates dolore facilis cumque corporis beatae, quia reprehenderit vero sint magnam, deserunt obcaecati praesentium ut doloribus reiciendis nesciunt unde temporibus!",
+    view: "Link at buraya",
+  },
+];
+
+export default WorkCardData;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,12 +3,15 @@ import Footer from "../components/Footer";
 import HeroImg2 from "../components/HeroImg2";
 import Navbar from "../components/Navbar";
 import ProjectCard from "../components/ProjectCard";
+import Work from "../components/Work";
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="PROJECTS" text="Some of my most recent works" />
+      <Work />
+      {/*simdi burada projectCard kalmayacak bunu kaldir*/}
       <ProjectCard />
       <Footer />
     </div>
