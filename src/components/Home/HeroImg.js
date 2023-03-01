55cfaa8953e0535928da4e6967c4cca4cc9bdc38
abc <duranakyol71@gmail.com>
2023-03-01 12:12:56 +0100

    add homepage responsive

--- src/components/Home/HeroImg.js
+++ src/components/Home/HeroImg.js
@@ -1,15 +1,12 @@
 import "./HeroImgStyles.css";
 import React from "react";
-import IntroImg from "../../assets/img/intro-bg.jpg";
+// import IntroImg from "../../assets/img/intro-bg.jpg";
 import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
-    <div className="hero">
-      <div className="mask">
-        <img className="into-img" src={IntroImg} alt="IntroImg" />
-      </div>
-      <div className="content">
+    <div className="hero-img1">
+      <div className="heading1">
         <p>HALLO, Ich bin Web Entwickler</p>
         <h1>Frontend Developer</h1>
         <div className="link">
